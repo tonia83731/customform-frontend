2a1f1f178ef8f8229cde24b2b2b776968263990f
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2025-03-10 11:17:46 +0800

    feat: add checked auth api

--- src/components/authentication/ProtectedRoute.tsx
+++ src/components/authentication/ProtectedRoute.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router";
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
+import { axiosFetch } from "../../api";
 
 interface ProtectedRouteProps {
   element: ReactElement;
@@ -7,8 +8,27 @@ interface ProtectedRouteProps {
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
   const authToken = localStorage.getItem("authToken");
+  const [isAuth, setIsAuth] = useState(null);
 
-  return authToken ? element : <Navigate to="/signin" replace />;
+  useEffect(() => {
+    if (!authToken) return;
+    const fetchCheckedAuth = async () => {
+      try {
+        const res = await axiosFetch("GET", "/auth");
+
+        if (res?.data.success) {
+          const { is_auth } = res?.data.data || {};
+          setIsAuth(is_auth);
+          if (!is_auth) localStorage.removeItem("authToken");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchCheckedAuth();
+  }, [authToken]);
+
+  return authToken && isAuth ? element : <Navigate to="/signin" replace />;
 };
 
 export default ProtectedRoute;
